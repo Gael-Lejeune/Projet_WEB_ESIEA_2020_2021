cb8977d03cd5896e4c18ba7a03681dfb48f74f66
Angélique P <72751313+angelique2000@users.noreply.github.com>
2021-04-28 11:19:44 +0200

    Update link.inc.php

--- model/link.inc.php
+++ model/link.inc.php
@@ -14,29 +14,53 @@ $InscriptionController='../controller/Inscription.php';
 
 // GESTION UTILISATEUR //
 
+$categories='Nom : Categories';
+$categoriesController='../controller/categories.php';
+$categoriesCSS='../html/css/style.css';
+$categoriesView='../view/categories_view.php';
+$categoriesModel='../processing/categories_processing.php';
+
+$commandes='Nom : Commandes';
+$commandesController='../controller/commande.php';
+$commandesCSS='../html/css/style.css';
+$commandesView='../view/commande_view.php';
+$commandesModel='../processing/commande_processing.php';
+
+$connexion='Nom : Connexion';
+$connexionController='../controller/connexion.php';
+$connexionCSS='../html/css/style.css';
+$connexionView='../view/connexion_view.php';
+$connexionModel='../processing/connexion_processing.php';
+
 $inscription='Nom : Inscription';
 $inscriptionController='../controller/formulaire_inscription.php';
 $inscriptionCSS='../html/css/style.css';
 $inscriptionView='../view/inscription_view.php';
 $inscriptionModel='../processing/inscription_processing.php';
 
+$pageAdmin='Nom : Administrateur';
+$pageAdminController='../controller/pageAdmin.php';
+$pageAdminCSS='../html/css/style.css';
+$pageAdminView='../view/pageAdmin_view.php';
+$pageAdminModel='../processing/pageAdmin_processing.php';
+
+$panier='Nom : Panier';
+$panierController='../controller/panier.php';
+$panierCSS='../html/css/style.css';
+$panierView='../view/panier_view.php';
+$panierModel='../processing/panier_processing.php';
+
+$produit='Nom : Produit';
+$produitController='../controller/produit.php';
+$produitCSS='../html/css/style.css';
+$produitView='../view/produit_view.php';
+$produitModel='../model/produit_model.php';
+
 $produitsListe='Nom : Produits';
 $produitsListeController='../controller/produitsListe.php';
 $produitsListeCSS='../html/css/style.css';
 $produitsListeView='../view/produitsListe_view.php';
 $produitsListeModel='../processing/produitsListe_processing.php';
-
-$categories='Nom : Produits';
-$categoriesController='../controller/categories.php';
-$categoriesCSS='../html/css/style.css';
-$categoriesView='../view/categories_view.php';
-$categoriesModel='../processing/categories_processing.php';
-
-$connexion='Nom : Produits';
-$connexionController='../controller/connexion.php';
-$connexionCSS='../html/css/style.css';
-$connexionView='../view/connexion_view.php';
-$connexionModel='../processing/connexion_processing.php';
 
 $logout='Se deconnecter';
 $logoutModel='../processing/logout_processing.php';
